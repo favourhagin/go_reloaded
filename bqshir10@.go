1c7b55badde73de28d7a20613ec342420aaac06c
bashirhassan10 <bqshir10@gmail.com>
2026-04-01 12:12:45 +0100

    created bqshir10@.go

---
+++ bqshir10@.go
@@ -0,0 +1,18 @@
+package main
+
+
+func fixArticles(text string) string {
+	word := strings.Fields(text)
+
+	for i := 0; i < len(word)-1; i++ {
+
+		if word[i] == "An" && !strings.ContainsAny(word[i+1][:1], "aeriouAERIOUHh") {
+			word[i] = "A"
+		}
+		if word[i] == "A" && strings.ContainsAny(word[i+1][:1], "aeriouAERIOUHh") {
+			word[i] = "An"
+		}
+	}
+	return strings.Join(word, " ")
+}
+

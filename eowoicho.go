3b202f43d45b855bb589b8451912ae9eac46e951
EkwuemeSpace <innocentekwueme79@gmail.com>
2026-04-01 17:33:30 +0100

    updated fixquote

--- eowoicho.go
+++ eowoicho.go
@@ -1,27 +1,27 @@
-package main
 func fixQuotes(input string) string {
 	inQuotes := false
+	quotes := ""
+	var start int
 	result := ""
-	quotes := rune(0)
-	for _, item := range input {
-		if !inQuotes && (item != '\'' && item != '"') {
-			result += string(item)
-		} else if !inQuotes && (item == '\'' || item == '"') {
-			quotes = item
-			result += string(item)
+
+	for i, char := range input {
+		if !inQuotes && char == '\'' {
+			quotes = string(char)
 			inQuotes = true
+			result += string(char)
+			start = i + 1
+			continue
 
-		} else if inQuotes && item == quotes {
-			result += string(item)
+		} else if inQuotes && string(char) == quotes {
+			inside_text := input[start:i]
+			inside_text = strings.TrimSpace(inside_text)
+			result = result + inside_text + string(char)
+
 			inQuotes = false
-
-		} else if inQuotes {
-			if item != ' ' {
-				result += string(item)
-			}
+			continue
+		} else if !inQuotes {
+			result += string(char)
 		}
 	}
-	return string(result)
+	return result
 }
-
-

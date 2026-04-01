8cf4a6f8d95adccb6656c6af48dcd3b7a268f911
joyadikwu2006 <joyadikwu2006@gmail.com>
2026-04-01 14:43:18 +0100

    Update jadikwu.go

--- jadikwu.go
+++ jadikwu.go
@@ -7,3 +7,17 @@ func ToLower(text string) string {
 	}
 	return string(runes)
 }
+
+// for go_reloaded 
+func ToLower(text string) string {
+	words := strings.Fields(text)
+	char := []string{}
+	for _, i := range words {
+		if i == "(low)" && len(char) > 0 {
+			char[len(char)-1] = strings.ToLower(char[len(char)-1])
+		} else if i != "(low)" {
+			char = append(char, i)
+		}
+	}
+	return strings.Join(char, " ")
+}

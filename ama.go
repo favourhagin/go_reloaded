63f4ce7252496b7f1036c8757ecb7926425b8b48
Aondowase1 <amaaondowase63@gmail.com>
2026-04-01 12:36:13 +0100

    Add files via upload

---
+++ ama.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func lowN(text string) string {
+	word := strings.Fields(text)
+
+	for i := len(word) - 1; i > 0; i-- {
+		if word[i] == "(low," {
+			numstr := strings.TrimSuffix(word[i+1], ")")
+			n, _ := strconv.Atoi(numstr)
+			for j:= i-n; j< i; j++ {
+			word[j]= strings.ToLower(word[j])
+			}
+			word = append(word[:i], word[i+2:]...)
+		}
+	}
+	return strings.Join(word, " ")
+}

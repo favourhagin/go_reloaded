50121f48807d61aefc0f136bec5481b53e9d5fa7
asean3033@gmail.com <asean3033@gmail.com>
2026-04-01 12:36:59 +0100

    fixPunctuation

---
+++ fixPunctuation/ashaibu.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func fixPunctuation(text string) string {
+	// remove spaces before .,!?;:
+	re := regexp.MustCompile(`\s+([.,!?;:])`)
+	text = re.ReplaceAllString(text, "$1")
+	return text
+}

5e1c1ea730b56900cafd914a9d2c9e6c8cb437c8
asean3033@gmail.com <asean3033@gmail.com>
2026-04-01 12:50:28 +0100

    fixPunctuation

--- fixPunctuation/ashaibu.go
+++ fixPunctuation/ashaibu.go
@@ -6,9 +6,16 @@ import (
 )
 
 func fixPunctuation(text string) string {
-	// remove spaces before .,!?;:
-	re := regexp.MustCompile(`\s+([.,!?;:])`)
+	// remove spaces before .,!'?;:
+	re := regexp.MustCompile(`\s+([.,!'?;:])`)
 	text = re.ReplaceAllString(text, "$1")
 	return text
 }
-func 
+func main() {
+	fmt.Println(fixPunctuation("I was thinking ... You were right!"))
+	fmt.Println(fixPunctuation("I was exactly how they described me'AWESOME'"))
+	fmt.Println(fixPunctuation(" I am coming to you '.     "))
+	fmt.Println(fixPunctuation("I was just , on my own    "))
+	fmt.Println(fixPunctuation("Dont you think you are crazy   ?"))
+
+}
